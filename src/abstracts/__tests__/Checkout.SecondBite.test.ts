a818fba067a8144a1d6db7458043d9df8db66da1
Aakash Bhatia <Aakash.Bhatia@vicinity.com.au>
2021-05-07 01:13:39 +1000

    cleanup

--- src/abstracts/__tests__/Checkout.SecondBite.test.ts
+++ src/abstracts/__tests__/Checkout.SecondBite.test.ts
@@ -28,33 +28,3 @@ describe('Checkout Class with a SecondBite Customer type', () => {
     expect(checkout.generateTotal(cartData)).toEqual(2549.92)
   })
 })
-
-describe('Checkout Class with a Axil Coffee Roasters Customer type', () => {
-  let checkout: CheckoutObject
-
-  beforeEach(() => {
-    checkout = new CheckoutObject(
-      pricingRules,
-      'Axil Coffee Roasters',
-      products
-    )
-    jest.spyOn(checkout, 'generateProductPrice')
-    jest.spyOn(checkout, 'generateTotal')
-  })
-
-  it('should calculate correct price for Classic Ad', () => {
-    expect(checkout.generateProductPrice(1, cartData)).toEqual(809.97)
-  })
-
-  it('should calculate correct price for Stand Out Ad', () => {
-    expect(checkout.generateProductPrice(2, cartData)).toEqual(1499.95)
-  })
-
-  it('should calculate correct price for Premium Ad', () => {
-    expect(checkout.generateProductPrice(3, cartData)).toEqual(394.99)
-  })
-
-  it('should calculate the total', () => {
-    expect(checkout.generateTotal(cartData)).toEqual(2704.91)
-  })
-})
