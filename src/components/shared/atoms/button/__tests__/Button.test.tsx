96602fe82ae833536a1f1bd348ccc0a849f83517
Aakash Bhatia <Aakash.Bhatia@vicinity.com.au>
2021-05-06 22:46:57 +1000

    button tests

--- src/components/shared/atoms/button/__tests__/Button.test.tsx
+++ src/components/shared/atoms/button/__tests__/Button.test.tsx
@@ -1,9 +1,16 @@
-import { render } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import { Button } from '../Button'
 
 describe('Button Component', () => {
   it('should render on page', () => {
-    render(<Button />)
-    expect(1).toBe(1)
+    render(<Button>Test my button</Button>)
+    expect(screen.getByText('Test my button')).toBeInTheDocument()
+  })
+
+  it('should fire the onClick function passed as a prop', () => {
+    const onClickMock = jest.fn()
+    render(<Button onClick={onClickMock}>Test onClick</Button>)
+    fireEvent.click(screen.getByText('Test onClick'))
+    expect(onClickMock).toHaveBeenCalledTimes(1)
   })
 })
